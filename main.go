ce35d3362ba367ec36577c3c86652aa629b08b17
huanwei <huan@harmonycloud.cn>
2018-08-08 10:02:10 +0800

    split programs
    Signed-off-by: huanwei <huan@harmonycloud.cn>

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/huanwei/kube-spy/app"
-
-func main() {
-	a := app.App{}
-	a.Initialize("DB_USERNAME", "DB_PASSWORD", "rest_api_demo")
-	a.Run(":8080")
-}
--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,17 +1,17 @@
-package app
+package api
 
 import (
 	"github.com/gorilla/mux"
 	client_v2 "github.com/influxdata/influxdb/client/v2"
 )
 
-type App struct {
+type ApiServer struct {
 	Router *mux.Router
 	InfluxDB     *client_v2.Client
 }
 
 // See https://github.com/influxdata/influxdb/tree/master/client
 
-func (a *App) Initialize(user, password, dbname string) {}
+func (a *ApiServer) Initialize(user, password, dbname string) {}
 
-func (a *App) Run(addr string) {}
+func (a *ApiServer) Run(addr string) {}

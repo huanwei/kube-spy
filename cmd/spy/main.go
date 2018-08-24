0043850d4368ee2d0ab6c02d517acf5ec9f3b43e
zjuzzb <1916317435@qq.com>
2018-08-24 11:12:23 +0800

    debugging

--- cmd/spy/main.go
+++ cmd/spy/main.go
@@ -58,6 +58,7 @@ func main() {
 			cidrs := spy.GetPod(clientset, services[i])
 			for _,cidr := range cidrs{
 				e := exec.New()
+				glog.Infof(fmt.Sprintf("ping"+cidr+"-i"+"0.01"+"-c"+"100"))
 				data,err := e.Command("ping",cidr,"-i","0.01","-c","100").CombinedOutput()
 				if err!= nil{
 					glog.Errorf(fmt.Sprintf("Failed to ping %s:%s",cidr,err))

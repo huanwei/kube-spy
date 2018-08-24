38686cbaa5cac2c2aee2dc25563a5fce48de63e1
zjuzzb <1916317435@qq.com>
2018-08-24 14:08:04 +0800

    use oping instead of ping in busybox

--- cmd/spy/main.go
+++ cmd/spy/main.go
@@ -61,10 +61,10 @@ func main() {
 			cidrs := spy.GetPod(clientset, services[i])
 			for _,cidr := range cidrs{
 				e := exec.New()
-				glog.Infof(fmt.Sprintf("ping"+cidr+"-i"+"0.01"+"-c"+"100"))
-				data,err := e.Command("ping","-i","0.01","-c","100",cidr).CombinedOutput()
+				glog.Infof(fmt.Sprintf("oping"+cidr+"-i"+"0.01"+"-c"+"100"))
+				data,err := e.Command("oping","-i","0.01","-c","100",cidr).CombinedOutput()
 				if err!= nil{
-					glog.Errorf(fmt.Sprintf("Failed to ping %s:%v",cidr,err.Error()))
+					glog.Errorf(fmt.Sprintf("Failed to ping %s:%s",cidr,err))
 				} else {
 					scanner := bufio.NewScanner(bytes.NewBuffer(data))
 					for scanner.Scan() {

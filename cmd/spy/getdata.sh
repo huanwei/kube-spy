62a9a580efe8551a081d24d23d98b7554da06296
BForever <11fhc@163.com>
2018-08-28 17:51:08 +0800

    use arg 1 to select recent data in getdata.sh

--- cmd/spy/getdata.sh
+++ cmd/spy/getdata.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+# arg 1: search time, example: 5m(5 minutes)
 # Get db address
 list=$(kubectl get pod -o wide|grep influxdb-spy)
 # Split string
 array=(${list// / })
 # Query
-curl -G "http://${array[5]}:8086/query?pretty=true" --data-urlencode "db=spy" --data-urlencode "q=SELECT * FROM response LIMIT 4"
-curl -G "http://${array[5]}:8086/query?pretty=true" --data-urlencode "db=spy" --data-urlencode "q=SELECT * FROM ping LIMIT 9"
+curl -G "http://${array[5]}:8086/query?pretty=true" --data-urlencode "db=spy" --data-urlencode "q=SELECT * FROM response WHERE time > now() - $1 LIMIT 4"
+curl -G "http://${array[5]}:8086/query?pretty=true" --data-urlencode "db=spy" --data-urlencode "q=SELECT * FROM ping WHERE time > now() - $1 LIMIT 9"

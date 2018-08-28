16b9509a6f81bc39a3a1dc10be2feac24c94075c
BForever <11fhc@163.com>
2018-08-28 15:00:17 +0800

    add limit on getdata.sh

--- cmd/spy/getdata.sh
+++ cmd/spy/getdata.sh
@@ -4,5 +4,5 @@ list=$(kubectl get pod -o wide|grep influxdb-spy)
 # Split string
 array=(${list// / })
 # Query
-curl -G "http://${array[5]}:8086/query?pretty=true" --data-urlencode "db=spy" --data-urlencode "q=SELECT * FROM response"
-curl -G "http://${array[5]}:8086/query?pretty=true" --data-urlencode "db=spy" --data-urlencode "q=SELECT * FROM ping"
+curl -G "http://${array[5]}:8086/query?pretty=true" --data-urlencode "db=spy" --data-urlencode "q=SELECT * FROM response LIMIT 4"
+curl -G "http://${array[5]}:8086/query?pretty=true" --data-urlencode "db=spy" --data-urlencode "q=SELECT * FROM ping 9"

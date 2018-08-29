f8965e34f3beaa05821b66f64ef19f9b7d46d52e
zjuzzb <1916317435@qq.com>
2018-08-29 14:18:48 +0800

    fix a bug of chaos not stored in db

--- cmd/spy/main.go
+++ cmd/spy/main.go
@@ -57,7 +57,7 @@ func main() {
 			}
 			// Detect network environment
 			cidrs, podNames := spy.GetPodsInfo(spy.GetPods(clientset, services[i]))
-			spy.PingPods(services[i].Name, services[i].Namespace, nil, podNames,cidrs)
+			spy.PingPods(services[i].Name, services[i].Namespace, nil, podNames, cidrs)
 			// Chaos tests
 			for _, chaos := range spyConfig.VictimServices[i].ChaosList {
 				glog.Infof("Chaos test: Victim %s, Chaos %v", spyConfig.VictimServices[i].Name, chaos)
@@ -70,13 +70,13 @@ func main() {
 				spy.Dotests(spyConfig, host, &spyConfig.VictimServices[i], &chaos)
 				// Detect network environment
 				cidrs, podNames := spy.GetPodsInfo(spy.GetPods(clientset, services[i]))
-				spy.PingPods(services[i].Name, services[i].Namespace, nil, podNames,cidrs)
+				spy.PingPods(services[i].Name, services[i].Namespace, &chaos, podNames, cidrs)
 				// Clear chaos
 				spy.ClearChaos(clientset, spyConfig)
 			}
 			// Detect network environment
 			cidrs, podNames = spy.GetPodsInfo(spy.GetPods(clientset, services[i]))
-			spy.PingPods(services[i].Name, services[i].Namespace, nil, podNames,cidrs)
+			spy.PingPods(services[i].Name, services[i].Namespace, nil, podNames, cidrs)
 		}
 
 	}

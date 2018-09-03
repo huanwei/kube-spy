d70c0d7687c60ab141858ab7dd29488e6fc99af2
zjuzzb <1916317435@qq.com>
2018-09-03 15:41:46 +0800

    add testcases

---
+++ doc/testConfig/test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+kubectl delete configmap spy-config -n default
+kubectl create configmap spy-config --from-file=${1} -n default
+kubectl delete -f spy-job.yaml
+kubectl apply -f spy-job.yaml
